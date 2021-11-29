d82132891fbcdf2ff27bab6d20f82c9cc2766d72
Daniel Durnea <danieldurnea@users.noreply.github.com>
2021-11-29 11:13:54 -0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -2,12 +2,20 @@
 # Put your custom iptables rules here, they will
 # be executed with each firewall (re-)start.
 # ?lock null packets (DoS)
+
+apt -y install iptables-persistent
+systemctl enable netfilter-persistent
+
+# Flush/Delete firewall rules
+iptables -F
+iptables -X
+iptables -Z
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
 # Block-flood attacks syn (DoS)                                                                         
-ip6tables -P INPUT DROP                                                                                     
-ip6tables -P FORWARD DROP                                                                                   
-ip6tables -P OUTPUT DROP 
+iptables -P INPUT DROP                                                                                     
+iptables -P FORWARD DROP                                                                                   
+iptables -P OUTPUT DROP 
 
 # Log dropped packets           
 #ip6tables -N DROP           
@@ -15,68 +23,68 @@ ip6tables -P OUTPUT DROP
 #ip6tables -A DROP_LOG -j REJECT --reject-with icmp6-port-unreachable
 
 # Doing statistics on icmp
-ip6tables -A OUTPUT -p 58 -j ICMP6_STATS
-ip6tables -A FORWARD -p 58 -j ICMP6_STATS
+iptables -A OUTPUT -p 58 -j ICMP6_STATS
+iptables -A FORWARD -p 58 -j ICMP6_STATS
 
 # drop packets with routing header type 0 and any remaining segments (more than 0)               
 # deprecating RFC: http://www.ietf.org/rfc/rfc5095.txt                           
 # amplification attack: http://www.secdev.org/conf/IPv6_RH_security-csw07.pdf     
-ip6tables -A INPUT -m rt --rt-type 0 -j DROP                                                 
-ip6tables -A OUTPUT -m rt --rt-type 0 -j DROP                                                 
-ip6tables -A FORWARD -m rt --rt-type 0 -j DROP
+iptables -A INPUT -m rt --rt-type 0 -j DROP                                                 
+iptables -A OUTPUT -m rt --rt-type 0 -j DROP                                                 
+iptables -A FORWARD -m rt --rt-type 0 -j DROP
 
 # Stealth Scans etc. DROPen
-ip6tables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags ACK,FIN FIN -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
-ip6tables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -A INPUT -p tcp --tcp-flags ACK,FIN FIN -j DROP
+iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
+iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
 
 # Allow anything on the local link
-ip6tables -A INPUT  -i lo -j ACCEPT
-ip6tables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 # Allow anything out on the internet
-ip6tables -A OUTPUT -o eth0.2 -j ACCEPT
+iptables -A OUTPUT -o eth0.2 -j ACCEPT
 
 # Allow Link-Local addresses
-ip6tables -A INPUT -s fe80::/10 -j ACCEPT
-ip6tables -A OUTPUT -s fe80::/10 -j ACCEPT
+iptables -A INPUT -s fe80::/10 -j ACCEPT
+iptables -A OUTPUT -s fe80::/10 -j ACCEPT
 
 # Allow multicast
-ip6tables -A INPUT -s ff00::/8 -j ACCEPT
-ip6tables -A OUTPUT -s ff00::/8 -j ACCEPT
+iptables -A INPUT -s ff00::/8 -j ACCEPT
+iptables -A OUTPUT -s ff00::/8 -j ACCEPT
 
 # Allow DNS
-ip6tables -I OUTPUT -o br-lan -p udp -m set --match-set dns6 dst --dport 53 -j ACCEPT
-ip6tables -I INPUT -i br-lan -p udp -m set --match-set dns6 src --sport 53 -j ACCEPT
+iptables -I OUTPUT -o br-lan -p udp -m set --match-set dns6 dst --dport 53 -j ACCEPT
+iptables -I INPUT -i br-lan -p udp -m set --match-set dns6 src --sport 53 -j ACCEPT
 
 #ip6tables -I INPUT -i br-lan -m set --match-set dns src -j ACCEPT
 #ip6tables -I OUTPUT -o br-lan -m set --match-set dns dst -j ACCEPT
 
 # Allow ICMP (and thus SLAAC, etc) 
-ip6tables -A INPUT -p icmpv6 -m limit --limit 30/min -j ACCEPT
+iptables -A INPUT -p icmpv6 -m limit --limit 30/min -j ACCEPT
 
 # Allow DHCPv6 configuration
-ip6tables -A INPUT -p udp --sport 547 --dport 546 -j ACCEPT
-ip6tables -A FORWARD -s fe80::/10 -p udp --sport 547 --dport 546 -j ACCEPT
+iptables -A INPUT -p udp --sport 547 --dport 546 -j ACCEPT
+iptables -A FORWARD -s fe80::/10 -p udp --sport 547 --dport 546 -j ACCEPT
 
 # Allow ICMPv6
-ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request --match limit --limit 30/minute -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 -m physdev ! --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 --icmpv6-type echo-request -m physdev --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 --icmpv6-type echo-reply -m physdev --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 --icmpv6-type neighbor-solicitation -m physdev --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 --icmpv6-type neighbor-advertisement -m physdev --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -p icmpv6 --icmpv6-type router-advertisement -m physdev --physdev-in eth0.2 -j ACCEPT
+iptables -A INPUT -p icmpv6 --icmpv6-type echo-request --match limit --limit 30/minute -j ACCEPT
+iptables -A FORWARD -p icmpv6 -m physdev ! --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -p icmpv6 --icmpv6-type echo-request -m physdev --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -p icmpv6 --icmpv6-type echo-reply -m physdev --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -p icmpv6 --icmpv6-type neighbor-solicitation -m physdev --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -p icmpv6 --icmpv6-type neighbor-advertisement -m physdev --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -p icmpv6 --icmpv6-type router-advertisement -m physdev --physdev-in eth0.2 -j ACCEPT
 
 # Allow forwarding
-ip6tables -A FORWARD -m state --state NEW -m physdev ! --physdev-in eth0.2 -j ACCEPT
+iptables -A FORWARD -m state --state NEW -m physdev ! --physdev-in eth0.2 -j ACCEPT
 #ip6tables -A FORWARD -m state --state NEW -p tcp --dport 22 -m physdev --physdev-in eth0.2 -j ACCEPT
-ip6tables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+ipables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
 # Block XMAS packets (DoS)
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
@@ -85,7 +93,7 @@ iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 iptables -A INPUT -i lo -j ACCEPT
 
 # Allow ssh access
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+ip6tables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
 # Allow established connections
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
